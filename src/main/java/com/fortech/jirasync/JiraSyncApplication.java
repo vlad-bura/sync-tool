71723bddbabd3c5afba25c2411bbe050e51e7cd5
Mihai Craciun <mihai.craciun@fortech.ro>
2023-07-04 14:49:05 +0300

    Jira Sync Tool skeleton

---
+++ src/main/java/com/fortech/jirasync/JiraSyncApplication.java
@@ -0,0 +1,13 @@
+package com.fortech.jirasync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiraSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fortech/jirasync/JiraSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fortech.jirasync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiraSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
